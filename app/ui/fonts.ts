e2065ea04a307775342771e5bfd07e2d9d709612
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2024-09-03 21:27:39 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Poppins, Rubik } from 'next/font/google';
+
+export const poppins = Poppins({ weight: ['400'], subsets: ['latin'] });
+export const rubik = Rubik({ weight: ["400"], subsets: ["latin"] });
+ 
+  
